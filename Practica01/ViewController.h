ec4abf8e9c85091e6e3ef24a6d44c248296a288c
ferbarragan <ferbarragan.developer@gmail.com>
2016-05-31 18:32:20 -0500

    Initial Commit

---
+++ Practica01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Practica01
+//
+//  Created by Christian Barragan on 31/05/16.
+//  Copyright © 2016 Christian Barragan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Practica01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Practica01
+//
+//  Created by Christian Barragan on 31/05/16.
+//  Copyright © 2016 Christian Barragan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
