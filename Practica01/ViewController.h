2f1670c907e2f07e3631933b0a4083008a3efe7f
ferbarragan <ferbarragan.developer@gmail.com>
2016-05-31 19:31:26 -0500

    Graphical Layout settled. Button outlets added.

--- Practica01/ViewController.h
+++ Practica01/ViewController.h
@@ -10,6 +10,28 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (weak, nonatomic) IBOutlet UIButton *walterDeadBtn;
+@property (weak, nonatomic) IBOutlet UIButton *walterLiveBtn;
+@property (weak, nonatomic) IBOutlet UIButton *gusDeadBtn;
+@property (weak, nonatomic) IBOutlet UIButton *gusLiveBtn;
+@property (weak, nonatomic) IBOutlet UIButton *hankDeadBtn;
+@property (weak, nonatomic) IBOutlet UIButton *hankLiveBtn;
+@property (weak, nonatomic) IBOutlet UIButton *lydiaDeadBtn;
+@property (weak, nonatomic) IBOutlet UIButton *lydiaLiveBtn;
+@property (weak, nonatomic) IBOutlet UIButton *jrDeadBtn;
+@property (weak, nonatomic) IBOutlet UIButton *jrLiveBtn;
+@property (weak, nonatomic) IBOutlet UIButton *jesseDeadBtn;
+@property (weak, nonatomic) IBOutlet UIButton *jesseLiveBtn;
+@property (weak, nonatomic) IBOutlet UIButton *saulDeadBtn;
+@property (weak, nonatomic) IBOutlet UIButton *saulLiveBtn;
+@property (weak, nonatomic) IBOutlet UIButton *toddDeadBtn;
+@property (weak, nonatomic) IBOutlet UIButton *toddLiveBtn;
+@property (weak, nonatomic) IBOutlet UIButton *mikeDeadBtn;
+@property (weak, nonatomic) IBOutlet UIButton *mikeLiveBtn;
+@property (weak, nonatomic) IBOutlet UIButton *skylerDeadBtn;
+@property (weak, nonatomic) IBOutlet UIButton *skylerLiveBtn;
 
 @end
 
--- Practica01/ViewController.m
+++ Practica01/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.scrollView setScrollEnabled:YES];
+    [self.scrollView setContentSize:CGSizeMake(320, 1700)];
 }
 
 - (void)didReceiveMemoryWarning {
